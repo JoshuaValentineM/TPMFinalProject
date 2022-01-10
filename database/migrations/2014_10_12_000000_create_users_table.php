3c092d41e16feb612311562c6f0dc341a0009c5e
JoshuaValentineM <joshuavalentinem@gmail.com>
2022-01-10 10:28:44 +0700

    Minor update user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('role');
+            $table->string('role')->default('member');
             $table->string('name');
             $table->string('email');
             $table->string('username');
